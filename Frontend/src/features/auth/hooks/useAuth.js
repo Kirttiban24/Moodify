e408fd87a2c70e5e31c59dc4225d7e532900e12c
Kirttiban24 <153419795+Kirttiban24@users.noreply.github.com>
2026-08-10 21:37:41 +0530

    update frontend

--- Frontend/src/features/auth/hooks/useAuth.js
+++ Frontend/src/features/auth/hooks/useAuth.js
@@ -1,5 +1,5 @@
 import { login, register, getMe, logout } from "../services/auth.api";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { AuthContext } from "../context/auth.context";
 
 export const useAuth = () => {
@@ -33,6 +33,10 @@ export const useAuth = () => {
         setUser(data.user)
         setLoading(false)
     }
+
+    useEffect(() => {
+        handleGetMe()
+    },[])
 
     return({
         user, loading, handleRegister, handleLogin, handleGetMe, handleLogout
