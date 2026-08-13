272b20c12b0d7ba7c49e717d0a5957f166c445db
Kirttiban24 <153419795+Kirttiban24@users.noreply.github.com>
2026-08-14 01:19:18 +0530

    imagekit integration add song upload and retrieval functionality with multer

--- Backend/src/controller/song.controller.js
+++ Backend/src/controller/song.controller.js
@@ -17,7 +17,7 @@ async function uploadSong(req, res){
             folder: "/moodify/songs"
         }),
         storageService.uploadFile({
-            buffer: tags.image.image,
+            buffer: tags.image.imageBuffer,
             filename: tags.title + ".jpeg",
             folder: "/moodify/posters"
         })
@@ -36,4 +36,17 @@ async function uploadSong(req, res){
     })
 }
 
-module.exports = { uploadSong }
+async function getSong(req, res) {
+    const { mood } = req.query
+
+    const song = await songModel.findOne({
+        mood
+    })
+
+    res.status(200).json({
+        message: "song fetched successfully",
+        song
+    })
+}
+
+module.exports = { uploadSong, getSong }
